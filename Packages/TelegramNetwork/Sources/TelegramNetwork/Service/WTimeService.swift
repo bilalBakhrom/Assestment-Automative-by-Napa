d496a15454c98caa2f93c2492608d53fd6671eef
Bilal <bilalbakhrom@gmail.com>
2022-10-26 21:55:12 +0500

    Fix time issue

--- Packages/TelegramNetwork/Sources/TelegramNetwork/Service/WTimeService.swift
+++ Packages/TelegramNetwork/Sources/TelegramNetwork/Service/WTimeService.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 final public class WTimeService: AdaptableNetwork<WTimeRouter> {
+    public override init(session: URLSession = URLSession.shared) {
+        let config = URLSessionConfiguration.ephemeral
+        let session = URLSession(configuration: config)
+        super.init(session: session)
+    }
+    
     public func fetchCurrentTime(completion: @escaping ServiceCompletion<WTime>) {
         bind(router: .timezone, completion: completion)
     }
